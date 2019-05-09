6c6067627e97dbf9c3eb692da1a61f1e8aa0b536
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-09 00:39:12 -0400

    read csv file attached for data

--- dash_test.py
+++ dash_test.py
@@ -1,4 +1,6 @@
-from dash import to_usd
+import os
+import pandas
+from dash import to_usd, top_selling_products
 
 def test_to_usd():
     assert to_usd(5) == "$5.00"
@@ -6,3 +8,10 @@ def test_to_usd():
     assert to_usd(3.889) == "$3.89"
 
     assert to_usd(3.8) =="$3.80"
+
+
+def test_top_selling_products():
+    csv_filename = "sales-201710.csv"
+    csv_filepath = os.path.join(os.path.dirname(__file__), "revisit-dash", csv_filename)
+    csv_data = pandas.read_csv(csv_filepath)
+    results = top_selling_products(csv_data)
