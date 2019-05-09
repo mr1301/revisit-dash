87ab4d7850079960d1776ad9abff231308d0df7e
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-09 00:07:15 -0400

    Get USD formating get outputs

--- dash.py
+++ dash.py
@@ -3,6 +3,9 @@ import pandas
 import plotly
 from plotly import graph_objs
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+    
 def top_selling_products(sales_data):
     product_totals = sales_data.groupby(["product"]).sum()
     product_totals = product_totals.sort_values("sales price", ascending=False)
@@ -20,10 +23,29 @@ if __name__ == "__main__":
 
     csv_filename =input("please input file name in this format, sales-201710.csv" )  # allow user to specify
 
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", csv_filename)
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "revisit-dash", csv_filename)
 
     csv_data = pandas.read_csv(csv_filepath)
     # CALCULATIONS
     monthly_total = csv_data["sales price"].sum()
 
     top_sellers = top_selling_products(csv_data)
+
+    # OUTPUTS
+
+    print("-----------------------")
+    print("MONTH: March 2018") # TODO: get month and year
+
+    print("-----------------------")
+    print("CRUNCHING THE DATA...")
+
+    print("-----------------------")
+    print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
+
+    print("-----------------------")
+    print("TOP SELLING PRODUCTS:")
+    for d in top_sellers:
+        print("  " + str(d["rank"]) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
+
+    print("-----------------------")
+    print("VISUALIZING THE DATA...")
