96b978621d1d69359bff5dd8ca8cc4759ab0c9ff
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-09 00:47:25 -0400

    input expected values from terminal

--- dash_test.py
+++ dash_test.py
@@ -15,3 +15,13 @@ def test_top_selling_products():
     csv_filepath = os.path.join(os.path.dirname(__file__), "revisit-dash", csv_filename)
     csv_data = pandas.read_csv(csv_filepath)
     results = top_selling_products(csv_data)
+
+    expected_result = [
+        {'rank': 1, 'name': 'Button-Down Shirt', 'monthly_sales': 5,464.20},
+        {'rank': 2, 'name': 'Super Soft Sweater', 'monthly_sales': 2,249.85},
+        {'rank': 3, 'name': 'Super Soft Hoodie', 'monthly_sales': 1800.00},
+        {'rank': 4, 'name': 'Khaki Pants', 'monthly_sales': 1780.00},
+        {'rank': 5, 'name': 'Vintage Logo Tee', 'monthly_sales': 526.35},
+        {'rank': 6, 'name': 'Sticker Pack', 'monthly_sales': 283.50},
+        {'rank': 7, 'name': 'Brown Boits', 'monthly_sales': 250.00}
+    ]
