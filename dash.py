3f12deebb32f1f0fba4797fc5878e317f33b3ef5
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-09 00:02:40 -0400

    define inputs csv & calculations

--- dash.py
+++ dash.py
@@ -13,3 +13,17 @@ def top_selling_products(sales_data):
         top_sellers.append(d)
         rank = rank + 1
     return top_sellers
+
+
+if __name__ == "__main__":
+    # INPUTS
+
+    csv_filename =input("please input file name in this format, sales-201710.csv" )  # allow user to specify
+
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", csv_filename)
+
+    csv_data = pandas.read_csv(csv_filepath)
+    # CALCULATIONS
+    monthly_total = csv_data["sales price"].sum()
+
+    top_sellers = top_selling_products(csv_data)
