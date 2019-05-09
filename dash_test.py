32232f9c9c66586d9a2c5a30268b3c388d77d386
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-09 01:05:47 -0400

    write the expected amounts from terminal

--- dash_test.py
+++ dash_test.py
@@ -11,17 +11,19 @@ def test_to_usd():
 
 
 def test_top_selling_products():
-    csv_filename = "sales-201710.csv"
-    csv_filepath = os.path.join(os.path.dirname(__file__), "revisit-dash", csv_filename)
+    csv_filename = "201710.csv"
+    csv_filepath = os.path.join(os.path.dirname(__file__), csv_filename)
     csv_data = pandas.read_csv(csv_filepath)
     results = top_selling_products(csv_data)
-
     expected_result = [
-        {'rank': 1, 'name': 'Button-Down Shirt', 'monthly_sales': 5,464.20},
-        {'rank': 2, 'name': 'Super Soft Sweater', 'monthly_sales': 2,249.85},
-        {'rank': 3, 'name': 'Super Soft Hoodie', 'monthly_sales': 1800.00},
-        {'rank': 4, 'name': 'Khaki Pants', 'monthly_sales': 1780.00},
+        {'rank': 1, 'name': 'Button-Down Shirt', 'monthly_sales': 5464.200000000001},
+        {'rank': 2, 'name': 'Super Soft Sweater', 'monthly_sales': 2249.8500000000004},
+        {'rank': 3, 'name': 'Super Soft Hoodie', 'monthly_sales': 1800.0},
+        {'rank': 4, 'name': 'Khaki Pants', 'monthly_sales': 1780.0},
         {'rank': 5, 'name': 'Vintage Logo Tee', 'monthly_sales': 526.35},
-        {'rank': 6, 'name': 'Sticker Pack', 'monthly_sales': 283.50},
-        {'rank': 7, 'name': 'Brown Boits', 'monthly_sales': 250.00}
+        {'rank': 6, 'name': 'Sticker Pack', 'monthly_sales': 283.5},
+        {'rank': 7, 'name': 'Brown Boots', 'monthly_sales': 250.0},
+        {'rank': 8, 'name': 'Winter Hat', 'monthly_sales': 155.4}
+
     ]
+    assert  results == expected_result
