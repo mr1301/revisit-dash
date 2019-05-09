b8683c720829c8b53033beca17d8807ac2945407
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-08 23:59:12 -0400

    define top_selling products

--- dash.py
+++ dash.py
@@ -2,3 +2,14 @@ import os
 import pandas
 import plotly
 from plotly import graph_objs
+
+def top_selling_products(sales_data):
+    product_totals = sales_data.groupby(["product"]).sum()
+    product_totals = product_totals.sort_values("sales price", ascending=False)
+    top_sellers = []
+    rank = 1
+    for i, row in product_totals.iterrows():
+        d = {"rank": rank, "name": row.name, "monthly_sales": row["sales price"]}
+        top_sellers.append(d)
+        rank = rank + 1
+    return top_sellers
